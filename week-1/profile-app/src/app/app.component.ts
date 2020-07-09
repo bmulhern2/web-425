68925063af4e13326fc0fbe3f658076f89a06d73
System Administrator <bmulhern09@gmail.com>
2020-07-09 13:00:41 -0400

    Deploy

---
+++ week-1/profile-app/src/app/app.component.ts
@@ -0,0 +1,19 @@
+/*
+============================================
+; Title: Assignment 1.5
+; Author: Professor Krasso
+; Date: 9 July 2020
+; Modified By: Brendan Mulhern
+; Description: This is the profile app.
+;====
+*/
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  assignment = "Assignment 1.5 - Components";
+}
