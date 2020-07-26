1b2f7fb877d780aaa72714a78064b6effb41797f
System Administrator <bmulhern09@gmail.com>
2020-07-26 09:32:34 -0400

    deploy

--- week-3/secure-profile-app/src/app/home/home.component.ts
+++ week-3/secure-profile-app/src/app/home/home.component.ts
@@ -1,15 +1,14 @@
 import { Component, OnInit } from '@angular/core';
-
+import { ActivatedRoute } from '@angular/router'
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-
-  constructor() { }
-
+  constructor(private route: ActivatedRoute) { 
+  }
+  this.isLoggedIn = Boolean(this.PerformanceResourceTiming.snapshot.queryParamMap.get('isloggedIn')
   ngOnInit(): void {
   }
-
 }
