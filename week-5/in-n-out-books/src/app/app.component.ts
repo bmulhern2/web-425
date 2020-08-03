3bd0ff89feaab90edad4579b7bc4a9c7e7029125
System Administrator <bmulhern09@gmail.com>
2020-08-03 13:45:47 -0400

    Deploy

---
+++ week-5/in-n-out-books/src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  assignment;
+  constructor() {
+    this.assignment = "Exercise 5.2 - Navigation and Layout"
+  }
+}
