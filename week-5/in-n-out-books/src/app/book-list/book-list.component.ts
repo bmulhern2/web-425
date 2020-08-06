67779de51c1663a50dc51aec2efa571b05a86f80
System Administrator <bmulhern09@gmail.com>
2020-08-06 13:12:37 -0400

    deploy

--- week-5/in-n-out-books/src/app/book-list/book-list.component.ts
+++ week-5/in-n-out-books/src/app/book-list/book-list.component.ts
@@ -1,3 +1,11 @@
+/*
+Title: Assignment 5.2 
+Author: Professor Krasso
+Date: 6 August 2020
+Modified By: Brendan Mulhern
+Description: It's the in-n-out book project!
+*/
+
 import { Component, OnInit } from '@angular/core';
 
 @Component({
--- week-5/in-n-out-books/src/app/book.interface.ts
+++ week-5/in-n-out-books/src/app/book.interface.ts
@@ -1,3 +1,11 @@
+/*
+Title: Assignment 5.2 
+Author: Professor Krasso
+Date: 6 August 2020
+Modified By: Brendan Mulhern
+Description: It's the in-n-out book project!
+*/
+
 export interface IBook {
     isbn: string;
     title: string;
